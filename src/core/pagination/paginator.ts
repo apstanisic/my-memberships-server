b10f3562307a49946b147d7156a45204ce0a4b46
Aleksandar Stanisic <aleksandar@tuta.io>
2019-09-04 00:02:48 +0200

    Fix code smells

--- src/core/pagination/paginator.ts
+++ src/core/pagination/paginator.ts
@@ -11,31 +11,17 @@ import { GenerateCursor } from './generate-cursor';
 import { HasId } from '../interfaces';
 
 /**
-<<<<<<< HEAD
  * Format is: uuid;columnName;columnValue;type
  * Part ";type" is optional.
  * So if we sort by createdAt, it will look like this
  * 8a4ba6e0-6dd9-4207-b179-6d922555d38e;createdAt;1567457902579
  * If column name ends with "At", value will be inferred to be date.
-=======
- * Format is:
- * column__value;id_value
- * So if we sort by createdAt, it will look like this
- * createdAt__1567457902579;id__8a4ba6e0-6dd9-4207-b179-6d922555d38e
- * Cursor always have 2nd field that is id. It is used to ensure uniquenes
->>>>>>> 689fcc0990449f776251d6ed6e408a7dc0159ef4
  * It will order and filter:
  * @example
  *  order: { createdAt: this.order },
  *  where: {
-<<<<<<< HEAD
  *    ...additionalFilter,
  *    ...parsedQuery // Automatically added
-=======
- *    createdAt: LessThan | MoreThan(1567457902579),
- *    id: MoreThan("8a4ba6e0-6dd9-4207-b179-6d922555d38e"),
- *    ...additionalFilter
->>>>>>> 689fcc0990449f776251d6ed6e408a7dc0159ef4
  *  },
  *  take: this.limit
  */
@@ -54,12 +40,9 @@ export class Paginator<T extends HasId> {
   /** Cursor provided from request. If null that means that it's first page */
   private cursor?: string;
 
-<<<<<<< HEAD
   /** Query from request. If filter is not provided, use this */
   private requestQuery: Record<string, any>;
 
-=======
->>>>>>> 689fcc0990449f776251d6ed6e408a7dc0159ef4
   constructor(repo: Repository<T>) {
     this.repo = repo;
   }
@@ -78,7 +61,6 @@ export class Paginator<T extends HasId> {
     this.limit = params.limit || this.limit;
     this.order = params.order || 'DESC';
     this.cursor = params.cursor;
-<<<<<<< HEAD
     this.requestQuery = params.query;
   }
 
@@ -87,30 +69,18 @@ export class Paginator<T extends HasId> {
     filter: Record<string, any> | null = null,
     useQuery = true,
   ): PaginationResponse<T> {
-=======
-  }
-
-  /* Execute query */
-  async execute(filter: {}): PaginationResponse<T> {
->>>>>>> 689fcc0990449f776251d6ed6e408a7dc0159ef4
     let cursorQuery;
     if (this.cursor) {
       cursorQuery = new ParseCursor(this.cursor).query;
     } else {
       cursorQuery = {};
     }
-<<<<<<< HEAD
     // If filter is not provided,
     // and user didn't forbid to use query, use query
     const whereQuery = filter && useQuery ? filter : this.requestQuery;
 
     const result = await this.repo.find({
       where: { ...whereQuery, ...cursorQuery },
-=======
-
-    const result = await this.repo.find({
-      where: { ...cursorQuery, ...filter },
->>>>>>> 689fcc0990449f776251d6ed6e408a7dc0159ef4
       order: { [this.orderColumn]: this.order },
       take: this.limit + 1,
     } as FindManyOptions<T>);
