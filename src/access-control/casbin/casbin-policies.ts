8556e0056d9e8d2ba5b1b85cc225b95d908d74ea
Aleksandar Stanisic <aleksandar@tuta.io>
2019-09-07 23:27:51 +0200

    Fix casbin error

--- src/access-control/casbin/casbin-policies.ts
+++ src/access-control/casbin/casbin-policies.ts
@@ -3,7 +3,7 @@
  * If this was a file it would have .csv extension
  */
 export const casbinPolicies = `
-p, app_owner, /*,  (read)|(write)read
+p, app_owner, /*,  (read)|(write)
 p, app_owner, /*, write
 
 p, app_admin, /companies/*,  (read)|(write)
@@ -13,8 +13,8 @@ p, app_admin, /users/*,  (read)|(write)
 p, owner, /companies/:id,  (read)|(write)
 p, owner, /companies/:id/*, (read)|(write)
 
-p, admin, /companies/:id/subscription, (read)|(write)
-p, admin, /companies/:id/subscription/*, (read)|(write)
+p, admin, /companies/:id/subscriptions, (read)|(write)
+p, admin, /companies/:id/subscriptions/*, (read)|(write)
 
 p, admin, /companies/:id/locations, (read)|(write)
 p, admin, /companies/:id/locations/*, (read)|(write)
