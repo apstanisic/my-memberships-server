70a2cc1a2cdaca4024e6d9681a84f222b76d4736
Aleksandar Stanisic <aleksandar@tuta.io>
2019-09-08 07:05:57 +0200

    Added .env file config and value checking

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '../config/config.service';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly configService: ConfigService) {}
-
   homePage(): string {
     return 'My subscriptions Api Home page';
   }
