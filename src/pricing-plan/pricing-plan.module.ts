2e576da4fffa203d5d55f566cdf95071d9d5d3a1
Aleksandar Stanisic <aleksandar@tuta.io>
2019-09-08 09:46:49 +0200

    Small fixes

--- src/pricing-plan/pricing-plan.module.ts
+++ src/pricing-plan/pricing-plan.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { PricingPlanService } from './pricing-plan.service';
 
 @Module({
-  providers: [PricingPlanService]
+  providers: [PricingPlanService],
 })
 export class PricingPlanModule {}
