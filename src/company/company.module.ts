50c1631909e89c86a7d04f86a15baee861df8819
Aleksandar Stanisic <aleksandar@tuta.io>
2019-10-04 14:34:24 +0200

    Add restrictions for creating roles

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -3,18 +3,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompanyService } from './company.service';
 import { Company } from './company.entity';
 import { CompaniesController } from './company.controller';
-import { CompaniesRolesModule } from '../companies-roles/companies-roles.module';
 import { CompanyImagesController } from './company-images/company-images.controller';
 import { CompanyImagesService } from './company-images/company-images.service';
 import { LocationsModule } from '../locations/locations.module';
+import { CompaniesRolesController } from './companies-roles.controller';
+import { AccessControlModule } from '../core/access-control/access-control.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Company]),
-    CompaniesRolesModule,
     LocationsModule,
+    AccessControlModule,
   ],
-  controllers: [CompaniesController, CompanyImagesController],
+  controllers: [
+    CompaniesController,
+    CompaniesRolesController,
+    CompanyImagesController,
+  ],
   providers: [CompanyService, CompanyImagesService],
   exports: [CompanyService],
 })
