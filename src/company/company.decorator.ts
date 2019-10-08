98cb43e6dde76936ef08517c2b6dd702953bfdaf
Aleksandar Stanisic <aleksandar@tuta.io>
2019-10-09 01:32:58 +0200

    Improve company decorator example

--- src/company/company.decorator.ts
+++ src/company/company.decorator.ts
@@ -3,6 +3,7 @@ import { createParamDecorator, UnauthorizedException } from '@nestjs/common';
 /**
  * Get company. This decorator can only be called after ValidCompanyGuard
  * @example
+ *  @UseGuards(CompanyLogsGuard)
  *  someMethod(@GetCompany() company: Company) {}
  */
 export const GetCompany = createParamDecorator((data: string, req) => {
